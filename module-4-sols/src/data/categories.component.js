468ce1e3d09eca055fd3a5ab249f552fc982dfe5
pushbhat <87753576+pushbhat@users.noreply.github.com>
2021-08-14 11:02:07 +0530

    url update

--- module-4-sols/src/data/categories.component.js
+++ module-4-sols/src/data/categories.component.js
@@ -3,7 +3,7 @@
 
   angular.module('MenuApp')
   .component('categories',{
-    templateUrl:'categories.template.html',
+    templateUrl:'src/data/categories.template.html',
     bindings:{
       items: '<',
     }
