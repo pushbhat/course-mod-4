57e73e0cc2c89e78fdc66316a3174463b55ad337
pushbhat <87753576+pushbhat@users.noreply.github.com>
2022-01-15 23:31:18 +0530

    Update common.module.js

--- mod5/src/common/common.module.js
+++ mod5/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'http://pushbhat-course.herokuapp.com')
+.constant('ApiPath', 'https://pushbhat-course.herokuapp.com')
 .config(config);
 
 config.$inject = ['$httpProvider'];
